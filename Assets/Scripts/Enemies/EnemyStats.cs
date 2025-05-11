2660f2959c30b42c6a3975b2a8153d8a80471f4a
pSenciales <124668501+pSenciales@users.noreply.github.com>
2025-05-11 13:17:00 +0200

    feat: Contador de enemigos

--- Assets/Scripts/Enemies/EnemyStats.cs
+++ Assets/Scripts/Enemies/EnemyStats.cs
@@ -5,7 +5,7 @@ public class EnemyStats : MonoBehaviour
     public int maxHealth = 20;
     private int currentHealth;
 
-    [Header("Curación")]
+    [Header("Curaciï¿½n")]
     public GameObject healingButtonPrefab;
     [Range(0f, 1f)] public float dropChance = 0.1f; // 10%
 
@@ -26,6 +26,13 @@ public class EnemyStats : MonoBehaviour
 
     void Die()
     {
+        var ui = FindObjectOfType<EnemyCount_UI>();
+        if (ui != null){
+            
+            ui.IncrementCount();
+
+        }
+
         TryDropHealing();
         Destroy(gameObject);
     }
